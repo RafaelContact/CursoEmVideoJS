3b4beeb3d3175575ffba45fe719704b9765aa370
RafaelContact <contactrodrigues21@gmail.com>
2022-04-30 18:59:07 -0300

    feat: adicionando cores e posicionamento

--- aula12/script.js
+++ aula12/script.js
@@ -6,9 +6,12 @@ var hour = date.getHours()
 msg.innerHTML = `Agora são ${hour} horas`
 if (hour >= 0 && hour < 12){
         img.src = '../asets/manha.png'
+        document.body.style.background = '#f3eedc'
     } else if ( hour >= 12 && hour < 18){
         img.src = '../asets/tarde.png'
+        document.body.style.background = '#fd5a01'
     } else {
         img.src = '../asets/noite.png'
+        document.body.style.background = '#241a1d'
 }
 }
