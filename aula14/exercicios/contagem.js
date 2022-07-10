542ff631a662369943144129fb950c850ed52996
RafaelContact <contactrodrigues21@gmail.com>
2022-07-10 19:10:27 -0300

    finalizando contagem progressiva

--- aula14/exercicios/contagem.js
+++ aula14/exercicios/contagem.js
@@ -1,6 +1,8 @@
 var btn = window.document.getElementById('btn')
 var res = window.document.getElementById('res')
 
+var searching = window.document.getElementById('searching')
+
 
 
 function text(){
@@ -8,14 +10,17 @@ function text(){
     var beggin = window.document.getElementById('beggin').value
     var end = window.document.getElementById('end').value
     var jump = window.document.getElementById('jump').value
+
+
+    searching.innerHTML = " Contando : "
     var result = Number(beggin)
      while ( result < Number(end)){
-         res.innerHTML += result   
-         res.innerHTML += " "
+         res.innerText += result   
+         res.innerText += ` \u{1F449} `
         result += Number(jump)
     }   
     if ( result == end){
-        res.innerHTML += result
+        res.innerHTML += ` ${result}`
     }
-
+    res.innerHTML += ` \u{1F3C1}`
 }
