57d2cb84f982b740d33039df577d07806886baa4
RafaelContact <contactrodrigues21@gmail.com>
2022-07-10 18:01:58 -0300

    adicionando contador

--- aula14/exercicios/contagem.js
+++ aula14/exercicios/contagem.js
@@ -1,12 +1,21 @@
-var beggin = window.document.getElementById('beggin').value
-var end = window.document.getElementById('end').value
-var jump = window.document.getElementById('jump').value
 var btn = window.document.getElementById('btn')
 var res = window.document.getElementById('res')
 
 
 
 function text(){
-    c = parseInt(beggin) + parseInt(end)
-    res.innerHTML = (c)
+    res.innerText = " "
+    var beggin = window.document.getElementById('beggin').value
+    var end = window.document.getElementById('end').value
+    var jump = window.document.getElementById('jump').value
+    var result = Number(beggin)
+     while ( result < Number(end)){
+         res.innerHTML += result   
+         res.innerHTML += " "
+        result += Number(jump)
+    }   
+    if ( result == end){
+        res.innerHTML += result
+    }
+
 }
