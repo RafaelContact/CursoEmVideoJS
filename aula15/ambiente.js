ee67eb89fe695221bd612868add747ecf2ecb248
RafaelContact <contactrodrigues21@gmail.com>
2022-07-11 22:04:07 -0300

    formas de usar o array e estrutura de repetição para posições

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,2 +1,10 @@
 let num = [0,5,8,4]
 console.log(`O vetor tem ${num.length} posições`)
+console.log(`O vetor é ${num}`)
+num[0] = 5
+num.push(2)
+console.log(`O vetor de forma crescente fica ${num.sort()}`)
+console.log(num[3])
+for(let pos=0; pos<num.length;pos++){
+    console.log(num[pos])
+}
