96cba7db5f27ddf87c72d096aa79418d39d8db25
RafaelContact <contactrodrigues21@gmail.com>
2022-07-11 22:18:44 -0300

    simplificando a estrutura for

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -8,3 +8,8 @@ console.log(num[3])
 for(let pos=0; pos<num.length;pos++){
     console.log(num[pos])
 }
+
+// simplificando 
+for (let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
