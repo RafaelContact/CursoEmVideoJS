88f3a5b6db0b117becf32ac4dc6bccf1e2050bad
RafaelContact <contactrodrigues21@gmail.com>
2022-07-16 19:12:15 -0300

    refazendo analizador de numeros, armazenando dentro de um array os valores apenas na primeira posição

--- desafio/script.js
+++ desafio/script.js
@@ -1,32 +1,39 @@
-var num = window.document.getElementById('num')
-var sel = window.document.getElementById('sel')
+//etapa um
 
-let values = []  // posso declarar o array vazio 
+//condições
+//valores acima de 100 ou abaixo de 1 são invalidos e retornam um alerta
+// caixa vazia retorna alerta
+// valores repitidos  não são adicionados.
 
-var btn = window.document.getElementById('btn')
+// funcionalidades 
+// adiciona atravez do evento onclick os numeros no select
+// são adicionados dentro de um array local
+// os numeros adicionados estão em uma template string com frase dentro do select
+// ao adicionar um número a caixa de digitação é zerada.
+
+// etapa 2
+
+// condições 
+// se eu adicionar outros valores , os resultados são apagados da tela aguardando o evento click novamente
+// se eu tentar finalizar antes de adicionar ele retorna um alert
+
+// funcionalidades 
+// atravez do evento clicar ele retorna os seguintes valores na tela: 
+// total de números cadastrados 
+// o menor valor 
+// a soma de todos os valores
+// a media dos valores digitados
+
+
+
+var sel = window.document.getElementById('sel') // select
+
+
+var btn = window.document.getElementById('btn') // botão de adicionar
 btn.addEventListener('click', add)
 
-function isNumber(n){
-    if(Numver(n) > 1 && Number(n) <= 100){
-        return true
-
-    }else{
-        return false
-    }
-}
-
-function inlist(n, l){
-    if (l.indexOf(Number(n) != -1 ) ){
-        return true 
-    }else { 
-        return false
-    }
-}
-
 function add(){
-    if (isNumber(num.value) && inlist(num.value, valores)){
-
-    } else {
-        window.alert('Valor invalido ou já encontrado na lista.')
-    }
+    var num = window.document.getElementById('num').value // input number, se eu declarar fora da função ela vai receber o valor do escopo global do carregamento
+    let values = [num]
+    console.log(values[0]) // aqui sempre vai receber na primeira posição
 }
