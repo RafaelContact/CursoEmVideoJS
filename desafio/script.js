b3f2c4bc37da17f12b12d112bb5201f92a360026
RafaelContact <contactrodrigues21@gmail.com>
2022-07-16 23:03:16 -0300

    o menor número cadastrado e o total de números

--- desafio/script.js
+++ desafio/script.js
@@ -1,25 +1,25 @@
 //etapa um
 
 //condições
-//valores acima de 100 ou abaixo de 1 são invalidos e retornam um alerta
-// caixa vazia retorna alerta
+//[x]valores acima de 100 ou abaixo de 1 são invalidos e retornam um alerta
+//[x] caixa vazia retorna alerta
 //[x] valores repitidos  não são adicionados.
 
 // funcionalidades 
-// adiciona atravez do evento onclick os numeros no select
+//[x] adiciona atravez do evento onclick os numeros no select
 //[x] são adicionados dentro de um array local
-// os numeros adicionados estão em uma template string com frase dentro do select
+//[x] os numeros adicionados estão em uma template string com frase dentro do select
 // ao adicionar um número a caixa de digitação é zerada.
 
 // etapa 2
 
 // condições 
-// se eu adicionar outros valores , os resultados são apagados da tela aguardando o evento click novamente
-// se eu tentar finalizar antes de adicionar ele retorna um alert
+// [x] se eu tentar finalizar antes de adicionar numeros ele retorna um alert
+// [x] se eu adicionar outros valores , os resultados são apagados da tela aguardando o evento click novamente
 
 // funcionalidades 
 // atravez do evento clicar ele retorna os seguintes valores na tela: 
-// total de números cadastrados 
+//[x] total de números cadastrados 
 // o menor valor 
 // a soma de todos os valores
 // a media dos valores digitados
@@ -30,20 +30,40 @@ var sel = window.document.getElementById('sel') // select
 var btn = window.document.getElementById('btn') // botão de adicionar
 var valores = [] // toda vez que um função retorna um valor, essa variável global acrescenta mais um
 x = 0 // essa variável se incrementada recebe o valor que vem da função
+var finish = window.document.getElementById('finish')
 btn.addEventListener('click', add)
 
 
 function add(){
     var num = window.document.getElementById('num').value // input number, se eu declarar fora da função ela vai receber o valor do escopo global do carregamento
     var print = valores.indexOf(num)
-    if( print == -1){
-        valores.push(num)
-        console.log(valores)
+    if(num > 0 && num < 100 ){
+        if( print == -1){
+            valores.push(num)
+            console.log(valores)
+            console.log(print)
+            var option = document.createElement('option')
+            option.innerHTML = `Adicionado número ${num}`
+            sel.appendChild(option)
+            res.innerHTML = ''
+            
+        }else{
+            window.alert('Valor já adicionado!')
+        }
     }else{
-        window.alert('Valor incorreto ou já adicionado!')
+        window.alert('Valor incorreto !')
     }
-    
-    console.log(print)
     
 }
 
+function result(){
+
+    if( valores.length <= 1){
+        window.alert('[ERRO] Digite outros números tente novamente!')
+    }else{
+        res.innerHTML = `O total de número cadastrados foi de ${valores.length} <br>`
+        valores.sort()
+        res.innerHTML += `O menor número cadastrado foi de ${valores[0]}`
+    }
+
+}
